acc9cd5ee288ad2edeb24cb424754fc8ab849796
Ian Johnson <tacoda@hey.com>
2025-01-03 14:14:25 +0100

    Add array methods

--- lib/reins.rb
+++ lib/reins.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-require_relative "reins/version"
+require "reins/version"
+require "reins/array"
 
 module Reins
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'content-type' => 'text/html'},
         ["Hello from Ruby on Reins!"]]
     end
---
+++ lib/reins/array.rb
@@ -0,0 +1,5 @@
+class Array
+    def deeply_empty?
+        empty? || all?(&:empty?)
+    end
+end
