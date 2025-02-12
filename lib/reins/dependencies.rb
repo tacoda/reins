a7f27acf2df29f100d462c53279171f634c20024
Ian Johnson <tacoda@hey.com>
2025-02-12 13:01:17 +0100

    Add name checks to const missing method

--- lib/reins/dependencies.rb
+++ lib/reins/dependencies.rb
@@ -1,11 +1,12 @@
 class Object
   def self.const_missing(c)
-    return nil if @calling_const_missing
+    @calling_const_missing ||= {}
+    return nil if @calling_const_missing[c]
 
-    @calling_const_missing = true
+    @calling_const_missing[c] = true
     require Reins.to_underscore(c.to_s)
     klass = Object.const_get(c)
-    @calling_const_missing = false
+    @calling_const_missing[c] = false
 
     klass
   end
